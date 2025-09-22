fc11fd4415fb3c6024d0fbd278ec68a27428519f
ijnamatsukaze75 <naromutajimav2@gmail.com>
2025-09-22 16:56:13 +0700

    Notes-app

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,35 @@
-console.log("hello");
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Koneksi database
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect(err => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("Connected to database");
+});
+
+// Route sederhana
+app.get("/", (req, res) => {
+  res.send("Backend API running 🚀");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
