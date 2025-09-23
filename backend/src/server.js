ca32400d19826781b2065d93cb6e8c47f973e44f
ijnamatsukaze75 <naromutajimav2@gmail.com>
2025-09-23 16:14:27 +0700

    menambahkan routes dan handlers

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,14 @@
-console.log("")
+import express from "express";
+import { testConnection } from "./config/db.js";
+import helloRoute from "./routes/helloRoute.js";
+
+const app = express();
+const port = 3000;
+
+//gunakan route
+app.use("/", helloRoute);
+
+app.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+    testConnection();
+});
