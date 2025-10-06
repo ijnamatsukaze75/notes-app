521674e8d1980dd80f2d924ca58abe4988f70e37
ijnamatsukaze75 <naromutajimav2@gmail.com>
2025-10-06 11:50:09 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import { testConnection } from "./config/db.js";
 import helloRoute from "./routes/helloRoute.js";
 import noteRouter from "./routes/notesRoute.js";
+import cors from "cors"
 
 const app = express();
-const port = 3000;
+
+app.use(cors());
 
 //gunakan route
 app.use(express.json());
@@ -12,7 +14,7 @@ app.use(express.json());
 app.use(helloRoute);
 app.use(noteRouter);
 
-
+const port = 3000;
 
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,94 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  const addNote = (title, content) => {
-    console.log(title);
-    console.log(content);
-    setNotes([]);
+  const fetchNotes = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/notes");
+
+      const result = await res.json();
+
+      setNotes(result.data);
+    } catch (error) {
+      console.error("Error", error);
+    }
   };
 
-  const handleDelete = (id) => {
-    console.log(id);
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const addNote = async (newTitle, newContent) => {
+    try {
+      const res = await fetch("http://localhost:3000/notes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: newTitle, content: newContent }),
+      });
+
+      const result = await res.json();
+
+      if (res.ok) {
+        setNotes([...notes, result.data]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:3000/notes/${id}`, {
+        method: "DELETE",
+      });
+
+      if(res.ok){
+        setNotes(( note ) => notes.filter((note) => note.id != id));
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const getNoteById = (id) => {
     console.log(id);
   };
 
-  const updateNote = (id, newTitle, newContent) => {
-    console.log(id);
-    console.log(newTitle);
-    console.log(newContent);
+  const handleupdateNote = async (id, updateTitle, updateContent) => {
+    try {
+      const res = await fetch(`http://localhost:3000/notes/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ title: updateTitle, content: updateContent }),
+      });
+
+      const result = await res.json();
+
+      setNotes((prevNotes) => {
+        return prevNotes.map((note) => (note.id === id ? result.data : note));
+      });
+
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <>
+    <div className="bg-[url(/ssv-33.jpg)] bg-no-repeat bg-cover">
       <Navbar />
-      <main className="min-h-screen flex flex-col mt-24 items-center">
+      <main className="min-h-screen flex flex-col items-center">
         <NoteForm onAddNote={addNote} />
         <NoteList
           notes={notes}
           onDelete={handleDelete}
-          onUpdate={updateNote}
+          onUpdate={handleupdateNote}
           onGetById={getNoteById}
         />
       </main>
-    </>
+    </div>
   );
 }
 
@@ -47,7 +100,7 @@ const Navbar = () => {
   return (
     <nav className="w-full fixed top-0 flex justify-center bg-white shadow">
       <div className="flex justify-between px-5 py-5 container">
-        <img src="/logo.svg" alt="Logo" />
+        <img src="/Horus-logo (2).svg" alt="Logo" />
       </div>
     </nav>
   );
@@ -65,7 +118,7 @@ const NoteForm = ({ onAddNote }) => {
   };
 
   return (
-    <section className="container max-w-xl px-5 mb-8">
+    <section className="container max-w-xl px-5 mb-8 mt-24">
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -84,7 +137,7 @@ const NoteForm = ({ onAddNote }) => {
         />
         <button
           type="submit"
-          className="bg-blue-500 text-white font-semibold rounded-lg py-3"
+          className="bg-[#57B6FF] text-white font-semibold rounded-lg py-3 opacity-85"
         >
           Add note
         </button>
@@ -94,30 +147,73 @@ const NoteForm = ({ onAddNote }) => {
 };
 
 const NoteItem = ({ note, onDelete, onUpdate }) => {
-  console.log(note);
-  console.log(onDelete);
-  console.log(onUpdate);
+  const [isEditing, setIsEditing] = useState(false);
+  const [titleEdit, setTitleEdit] = useState(note.title);
+  const [contentEdit, setContentEdit] = useState(note.content);
+
+  const handleCancel = () => {
+    setTitleEdit(note.title);
+    setContentEdit(note.content);
+    setIsEditing(false);
+  };
 
   return (
-    <div className="rounded-lg shadow-md bg-white w-[300px] p-5">
-      <p className="font-medium text-xl">{note.title}</p>
-      <p className="text-sm text-gray-500">
-        ~{showFormattedDate(note.createAt)}
-      </p>
-      <p className="mt-2">{note.content}</p>
-      <div className="mt-4 flex gap-2">
-        <button className="bg-yellow-500 text-white px-3 py-1 rounded">
-          Edit
-        </button>
-        <button className="bg-red-500 text-white px-3 py-1 rounded">
-          Delete
-        </button>
-      </div>
+    <div className="rounded-lg shadow-md bg-white w-[300px] p-5 opacity-90">
+      {isEditing ? (
+        <>
+          <input
+          value={titleEdit}
+            type="text"
+            className="w-full rounded-sm outline outline-gray-400 p-2"
+            onChange={(e) => setTitleEdit(e.target.value)}
+          />
+
+          <textarea
+          value={contentEdit}
+            type="text"
+            className="w-full rounded-sm outline outline-gray-400 p-2 mt-2"
+            onChange={(e) => setContentEdit(e.target.value)}
+          />
+
+          <div className="mt-4 flex gap-2">
+            <button className="bg-gray-500 text-white px-3 py-1 rounded"
+            onClick={handleCancel}>
+              Cancel
+            </button>
+            <button className="bg-green-500 text-white px-3 py-1 rounded"
+            onClick={() => {
+              onUpdate(note.id, titleEdit, contentEdit);
+              setIsEditing(false);
+            }}>
+              Save
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <p className="font-medium text-xl">{note.title}</p>
+          <p className="text-sm text-gray-500">
+            ~{showFormattedDate(note.created_at)}
+          </p>
+          <p className="mt-2">{note.content}</p>
+          <div className="mt-4 flex gap-2">
+            <button className="bg-yellow-500 text-white px-3 py-1 rounded"
+            onClick={() => setIsEditing(true)}
+            >
+              Edit
+            </button>
+            <button className="bg-red-500 text-white px-3 py-1 rounded"
+            onClick={() => onDelete(note.id)}>
+              Delete
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, onUpdate, onDelete }) => {
   return (
     <section className="container py-8">
       <h2 className="inline-flex items-center gap-2 text-2xl font-medium mb-6">
@@ -126,7 +222,7 @@ const NoteList = ({ notes }) => {
       </h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {notes.length > 0 ? (
-          notes.map((note) => <NoteItem key={note.id} note={note} />)
+          notes.map((note) => <NoteItem key={note.id} note={note} onUpdate={onUpdate} onDelete={onDelete} />)
         ) : (
           <h1>Data Kosong</h1>
         )}
